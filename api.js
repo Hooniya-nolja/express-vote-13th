fbb9355c207a45f67ca08234960926ecd7370295
Hooniyanolja <wkdckdgns01@naver.com>
2021-05-09 00:15:23 +0900

    fix: Add vote API

--- api.js
+++ api.js
@@ -5,21 +5,46 @@ const cors = require('cors');
 app.use(cors());
 
 const serverPort = 8000;
-const candidates = [
+let candidates = [
     {
-        id: 1,
+        id: 0,
         name: '김선종',
         voteCount: 120
     },
     {
-        id: 2,
+        id: 1,
         name: '박예진',
         voteCount: 160
     },
     {
-        id: 3,
+        id: 2,
         name: '안건희',
         voteCount: 200
+    },
+    {
+        id: 3,
+        name: '이승범',
+        voteCount: 132
+    },
+    {
+        id: 4,
+        name: '김영우',
+        voteCount: 186
+    },
+    {
+        id: 5,
+        name: '이호연',
+        voteCount: 154
+    },
+    {
+        id: 6,
+        name: '이소정',
+        voteCount: 199
+    },
+    {
+        id: 7,
+        name: '정윤선',
+        voteCount: 201
     }
 ]
 
@@ -29,4 +54,16 @@ const server = app.listen(serverPort, () => {
 
 app.get('/api/candidates', async (req, res) => {
     res.send(candidates);
+});
+
+app.get('/api/vote', async (req, res) => {
+    let voteId = req.query.id;
+    if(candidates[voteId]) {
+        // console.log('Before voteCount : ', candidates[voteId - 1].voteCount);
+        candidates[voteId].voteCount++;
+        // console.log('After voteCount : ', candidates[voteId - 1].voteCount);
+        res.status(200).send(`Successfully Voted to ${candidates[voteId].name}`);
+    } else {
+        res.status(404).send('Candidate does not exist');
+    }
 });
