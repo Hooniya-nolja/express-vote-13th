696827cd6cf1b58b530b82f8a598af773dac86c2
Hooniyanolja <wkdckdgns01@naver.com>
2021-05-21 16:30:30 +0900

    feat: Add signin and jwt authorization

--- api.js
+++ api.js
@@ -11,64 +11,38 @@ app.use(cors());
 app.use(bodyParser.json());
 require('dotenv').config();
 
-// let candidates = [
-//     {
-//         id: 0,
-//         name: '김선종',
-//         voteCount: 120
-//     },
-//     {
-//         id: 1,
-//         name: '박예진',
-//         voteCount: 160
-//     },
-//     {
-//         id: 2,
-//         name: '안건희',
-//         voteCount: 200
-//     },
-//     {
-//         id: 3,
-//         name: '이승범',
-//         voteCount: 132
-//     },
-//     {
-//         id: 4,
-//         name: '김영우',
-//         voteCount: 186
-//     },
-//     {
-//         id: 5,
-//         name: '이호연',
-//         voteCount: 154
-//     },
-//     {
-//         id: 6,
-//         name: '이소정',
-//         voteCount: 199
-//     },
-//     {
-//         id: 7,
-//         name: '정윤선',
-//         voteCount: 201
-//     }
-// ]
-
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const PORT = process.env.SERVER_PORT || 8000;
+
 const server = app.listen(PORT, () => {
     console.log(`Start Server : localhost:${PORT}`);
 });
+
+const verifyToken = (req, res, next) => {
+    console.log('VerityToken Ing!!!');
+    try {
+        const clientToken = req.headers['authorization'];
+        const decodedToken = jwt.verify(clientToken, JWT_SECRET_KEY);
+
+        if (decodedToken){
+            // res.locals.loginEmail = decoded.email;
+            next();
+        } else {
+            res.status(401).send('Unauthorized');
+        }
+    } catch (err){
+        res.status(401).send('Token Expired');
+    }
+};
 
 app.get('/api/candidates', async (req, res) => {
     res.send(candidates);
 });
 
-app.get('/api/vote', async (req, res) => {
+app.get('/api/vote', verifyToken, async (req, res) => {
     let voteId = req.query.id;
     if(candidates[voteId]) {
-        // console.log('Before voteCount : ', candidates[voteId - 1].voteCount);
         candidates[voteId].voteCount++;
-        // console.log('After voteCount : ', candidates[voteId - 1].voteCount);
         res.status(200).send(`Successfully Voted to ${candidates[voteId].name}`);
     } else {
         res.status(404).send('Candidate does not exist');
@@ -76,18 +50,13 @@ app.get('/api/vote', async (req, res) => {
 });
 
 app.post('/api/signup', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let name = req.body.name;
+    let loginUser = users.filter(user => user.email == email);
     try {
-        console.log('body : ', req.body);
-
-        let email = req.body.email;
-        let password = req.body.password;
-        let name = req.body.name;
-
         if (email && password && name){
-            let userLogin = users.filter(user => user.email == email);
-
-            if (userLogin.length){
-                console.log('users : ', users);
+            if (loginUser.length){
                 res.status(409).send('already exist Email');
             } else {
                 users.push({
@@ -95,7 +64,6 @@ app.post('/api/signup', async (req, res) => {
                     password: password,
                     name: name
                 });
-                console.log('users : ', users);
                 res.status(201).send('Succesfully Generated');
             }
         } else {
@@ -105,3 +73,31 @@ app.post('/api/signup', async (req, res) => {
         console.log('ERROR : ', err);
     }
 });
+
+app.post('/api/signin', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let loginUser = users.filter(user => user.email == email);
+
+    try {
+        if (email && password){
+            if (loginUser.length && loginUser[0].password == password){
+                const token = jwt.sign({
+                    email: email
+                }, JWT_SECRET_KEY, {
+                    expiresIn: '12h'
+                });
+        
+                // res.cookie('user', token);
+                res.status(201).send(token);
+            } else {
+                res.status(404).send('Login Failed');
+            }
+        } else {
+            res.status(400).send('Bad Request');
+        }
+    } catch (err){
+        console.log('ERROR : ', err);
+    }
+});
+
